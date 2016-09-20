7d959a76f8b35c244bc4a3afc052df8afd46be76
TsarVasiliy <tsar.vasiliy@mail.ru>
2016-09-20 16:44:38 +0800

    Test

--- Test/ConsoleApplication1/Calc.cs
+++ Test/ConsoleApplication1/Calc.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
-    class Calc
+    public class Calc
     {
-        
+      
        public double sum(double x, double y) { return x + y; }
        public double sub(double x, double y) { return x - y; }
        public double mul(double x, double y) { return x * y; }
---
+++ Test/Test/Class1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ConsoleApplication1;
+
+namespace Test
+{
+    public class Class1
+    {
+
+        [Test]
+        public void SumPositiveNumbers()
+        {   
+            
+            var calc = new Calc();
+            double first = 12;
+            double second = 3;
+            Assert.AreEqual(15, calc.sum(first, second));
+        }
+
+
+        [Test]
+        public void SumNegativeNumbers()
+        {
+
+            var calc = new Calc();
+            double first = -12;
+            double second = -3;
+            Assert.AreEqual(-15, calc.sum(first, second));
+        }
+
+
+        [Test]
+        public void Sumfailed()
+        {
+
+            var calc = new Calc();
+            double first = 1;
+            double second = 3;
+            Assert.AreEqual(15, calc.sum(first, second));
+        }
+
+        [Test]
+        public void SubPositiveNumbers()
+        {
+
+            var calc = new Calc();
+            double first = 4;
+            double second = 1;
+            Assert.AreEqual(3, calc.sub(first, second));
+        }
+
+        [Test]
+        public void SubNegatineNumbers()
+        {
+
+            var calc = new Calc();
+            double first = -4;
+            double second = -1;
+            Assert.AreEqual(-3, calc.sub(first, second));
+        }
+
+        [Test]
+        public void SubFaled()
+        {
+
+            var calc = new Calc();
+            double first = -4;
+            double second = -1;
+            Assert.AreEqual(3, calc.sub(first, second));
+        }
+
+        [Test]
+        public void MulPositiveNumbers()
+        {
+
+            var calc = new Calc();
+            double first = 5;
+            double second = 6;
+            Assert.AreEqual(30, calc.mul(first, second));
+        }
+
+        [Test]
+        public void MulNegativeNumbers()
+        {
+
+            var calc = new Calc();
+            double first = -5;
+            double second = -6;
+            Assert.AreEqual(30, calc.mul(first, second));
+        }
+
+        [Test]
+        public void MulFailed()
+        {
+
+            var calc = new Calc();
+            double first = -5;
+            double second = -6;
+            Assert.AreEqual(-30, calc.mul(first, second));
+        }
+
+        [Test]
+        public void DivPositiveNumbers()
+        {
+
+            var calc = new Calc();
+            double first = 20;
+            double second = 5;
+            Assert.AreEqual(4, calc.div(first, second));
+        }
+
+        [Test]
+        public void DivNegativeNumbers()
+        {
+
+            var calc = new Calc();
+            double first = -20;
+            double second = -4;
+            Assert.AreEqual(5, calc.div(first, second));
+        }
+
+        [Test]
+        public void Divfailed()
+        {
+
+            var calc = new Calc();
+            double first = 20;
+            double second = 5;
+            Assert.AreEqual(8, calc.div(first, second));
+        }
+    }
+}
